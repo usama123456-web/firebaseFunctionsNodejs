343e636fa9db113981d418f0a328615e2cfc8098
faizan123456 <faizydhillon28@gmail.com>
2021-10-15 17:18:09 +0500

    check tests

--- automationTestingNodejs/controllers/testingAutomationController.js
+++ automationTestingNodejs/controllers/testingAutomationController.js
@@ -11,5 +11,16 @@ exports.testing=(async (req,res,next)=>{
     } finally {
       await driver.quit();
     }
+})
 
+exports.validation=(async (req,res,next)=>{
+  let driver = await new Builder().forBrowser('chrome').build();
+  try {
+    await driver.get('http://www.google.com/ncr');
+    await driver.findElement(By.name('q')).sendKeys('howto', Key.RETURN);
+    await driver.wait(until.titleIs('pogba'), 1000);
+  } 
+   finally {
+    await driver.quit();
+  }
 })
--- automationTestingNodejs/routes/testingRouter.js
+++ automationTestingNodejs/routes/testingRouter.js
@@ -6,7 +6,7 @@ const router=express.Router()
 router
 .route('/testingSelenium')
 .get(testingAutomationController.testing)
-
+.get(testingAutomationController.validation)
 
 
 module.exports=router
